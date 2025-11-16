7f6cee8e4f7fdc7e64a4059bdf2767af8b4a227b
Teosork <76114875+Teosork@users.noreply.github.com>
2025-11-16 18:52:12 +0100

    Correcting math equations and enabling tests

--- 04_mathEquations/mathEquations.js
+++ 04_mathEquations/mathEquations.js
@@ -1,9 +1,9 @@
 const a = 1 - 1 // Freebie!!! This is the answer to "one minus one"
-const b = "one plus eight"
-const c = "22 times three"
-const d = "the *remainder* of 5/4"
-const e = "the variable 'b' minus 17"
-const f = "the sum of the previous five variables"
+const b = 1 + 8 // This is the answer to "one plus eight"
+const c = 22 * 3 // This is the answer to "twenty-two multiplied by three"
+const d = 5 % 4 // This is the answer to "the remainder of five divided by four"
+const e = b - 17 // This is the answer to "seventeen less than variable b"
+const f = a + b + c + d + e // This is the answer to "the sum of all the variables (a, b, c, d, and e)"
 
 // Do not edit below this line
 module.exports = {a, b, c, d, e, f}
