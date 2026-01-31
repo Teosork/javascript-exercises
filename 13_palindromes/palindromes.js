0f2d16b6df093dbf032c28e4b8afee7b9d183b0a
Teosork <76114875+Teosork@users.noreply.github.com>
2026-01-31 18:44:33 +0100

    Creating the function and testing it

--- 13_palindromes/palindromes.js
+++ 13_palindromes/palindromes.js
@@ -1,6 +1,8 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+    const lowerCaseStr = string.toLowerCase();
+    const modifiedStr = lowerCaseStr.replace(/[\W_]/g, '');
+    const reversedStr = modifiedStr.split('').reverse().join('');
+    return modifiedStr === reversedStr;
 };
-
 // Do not edit below this line
 module.exports = palindromes;
