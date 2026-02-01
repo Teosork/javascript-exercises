648743ac78c133e6a9a6d868302dea10eba4ab1e
Teosork <76114875+Teosork@users.noreply.github.com>
2026-02-01 20:14:02 +0100

    Writing the function and testing it

--- 16_findTheOldest/findTheOldest.js
+++ 16_findTheOldest/findTheOldest.js
@@ -1,6 +1,18 @@
-const findTheOldest = function() {
+const findTheOldest = function(people){
+    const currentYear = (new Date()).getFullYear();
 
-};
+    return people.reduce((oldest, person) =>{
+    const deathYear = person.yearOfDeath ?? currentYear
+    const agePerson = deathYear - person.yearOfBirth
+    const deathYearOldest = oldest.yearOfDeath ?? currentYear
+    const ageOldest = deathYearOldest - oldest.yearOfBirth
+    if (agePerson > ageOldest) {
+        return person;
+    }else {
+        return oldest;
+    }
+    }, people[0]);
+}
 
 // Do not edit below this line
 module.exports = findTheOldest;
