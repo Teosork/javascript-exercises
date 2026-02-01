86961f21aa5b902590a30354642f8e21a6a2166a
Teosork <76114875+Teosork@users.noreply.github.com>
2026-02-01 19:35:58 +0100

    Writing the function and testing it

--- 15_getTheTitles/getTheTitles.js
+++ 15_getTheTitles/getTheTitles.js
@@ -1,6 +1,8 @@
-const getTheTitles = function() {
-
-};
+function getTheTitles(array) {
+  return array.map(function (book) {
+    return book.title;
+  });
+}
 
 // Do not edit below this line
 module.exports = getTheTitles;
