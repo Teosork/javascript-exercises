144356ceed5d386726a338aace7e59a3c972855b
Teosork <76114875+Teosork@users.noreply.github.com>
2026-02-01 14:46:55 +0100

    Creating the function and testing it

--- 14_fibonacci/fibonacci.js
+++ 14_fibonacci/fibonacci.js
@@ -1,4 +1,25 @@
-const fibonacci = function() {
+const fibonacci = function(arg) {
+    let n;
+    
+    if (typeof arg !== "number"){
+        n = parseInt(arg, 10);
+    } else {
+        n = arg;
+    }
+
+    let firstPrev = 1;
+    let secondPrev = 0;
+
+    if (n < 0) return "OOPS";
+    if (n === 0) return 0;
+   
+    for (let i = 2; i <= n; i++) {
+        let current = firstPrev + secondPrev;
+        secondPrev = firstPrev;
+        firstPrev = current;
+    }
+
+    return firstPrev;
 
 };
 
